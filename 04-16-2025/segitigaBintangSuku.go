f917bba2afa7b35952a808bd36045499688ecc2f
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-04-16 01:02:44 +0700

    1

---
+++ 04-16-2025/segitigaBintangSuku.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("Masukkan jumlah baris: ")
+	fmt.Scan(&n)
+
+	// Mulai dari sini kamu isi dengan loop-nya
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println(" ")
+	}
+}
