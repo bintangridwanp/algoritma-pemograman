2c719e29f48f9696300c76a63aa94a637f014553
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-04-20 21:32:04 +0700

    5

---
+++ 20-04-2025/palindromChecker.go
@@ -0,0 +1,19 @@
+package main1
+
+import "fmt"
+
+func main1() {
+
+	var input string;
+
+	fmt.Print("Masukan Kalimat: ")
+	fmt.Scanln(&input)
+
+	for i:=  0 ; i < len(input)/2; i++ {
+		if input[i] != input[len(input)-i-1]{
+			fmt.Println("Not a palindrome")
+			return
+		}
+	}
+	fmt.Println("Palindrome")
+}	
