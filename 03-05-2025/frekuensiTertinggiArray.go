6887f556d5d5eb54a1684fe27c65c7bc00289149
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-05-10 22:09:44 +0700

    28

---
+++ 03-05-2025/frekuensiTertinggiArray.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	angka := [...]int{1, 2, 2, 3, 3, 3, 4, 2}
+
+	temp := angka[0]
+
+	for i := 0; i < len(angka); i++ {
+		for j := i + 1; j < len(angka); j++ {
+
+		}
+	}
+
+}
