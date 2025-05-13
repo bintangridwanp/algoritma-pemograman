b3f5e5ae23c0e4ff8b527be62ecd1863d15d7bf3
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-05-13 22:27:13 +0700

    29

--- 03-05-2025/frekuensiTertinggiArray.go
+++ 03-05-2025/frekuensiTertinggiArray.go
@@ -3,15 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	data := [...]int{1, 2, 2, 3, 3, 3, 4, 2}
 
-	angka := [...]int{1, 2, 2, 3, 3, 3, 4, 2}
+	var frekuensi [101]int // Asumsikan nilai dalam array <= 100
 
-	temp := angka[0]
+	// Hitung frekuensi setiap nilai
+	for i := 0; i < len(data); i++ {
+		frekuensi[data[i]]++
+	}
 
-	for i := 0; i < len(angka); i++ {
-		for j := i + 1; j < len(angka); j++ {
-
+	// Cari nilai frekuensi maksimum
+	maks := 0
+	for i := 0; i < len(frekuensi); i++ {
+		if frekuensi[i] > maks {
+			maks = frekuensi[i]
 		}
 	}
 
+	// Cetak semua nilai dengan frekuensi tertinggi
+	fmt.Println("Nilai paling sering muncul:")
+	for i := 0; i < len(frekuensi); i++ {
+		if frekuensi[i] == maks {
+			fmt.Printf("%d (muncul %d kali)\n", i, maks)
+		}
+	}
 }
