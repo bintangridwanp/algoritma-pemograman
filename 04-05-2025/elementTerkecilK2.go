1d283ae42f28114de41d0a4edb2357f5b80b5bfa
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-05-19 23:40:53 +0700

    32

--- 04-05-2025/elementTerkecilK2.go
+++ 04-05-2025/elementTerkecilK2.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func main() {
+	data := [...]int{8, 3, 7, 1, 4}
+
+	// Inisialisasi dengan nilai maksimum dari integer
+	min1 := data[0]
+	min2 := 9999999 // asumsi angka sangat besar
+
+	// Cari nilai terkecil pertama
+	for i := 1; i < len(data); i++ {
+		if data[i] < min1 {
+			min1 = data[i]
+		}
+	}
+
+	// Cari nilai terkecil ke-2 (lebih besar dari min1)
+	for i := 0; i < len(data); i++ {
+		if data[i] > min1 && data[i] < min2 {
+			min2 = data[i]
+		}
+	}
+
+	fmt.Println("Nilai terkecil ke-2 adalah:", min2)
+}
--- 04-05-2025/palindromArray.go
+++ 04-05-2025/palindromArray.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	angka := []int{1, 2, 3, 2, 2}
+	palindrome := true
+
+	for i := 0; i < len(angka)/2; i++ {
+		if angka[i] != angka[len(angka)-1-i] {
+			palindrome = false
+			break
+		}
+	}
+
+	if palindrome {
+		fmt.Println("Array adalah palindrome")
+	} else {
+		fmt.Println("Array bukan palindrome")
+	}
+
+}
