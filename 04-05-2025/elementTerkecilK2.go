8959ab9fcb1134b4db639e3dc435a2596fb9f525
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-05-20 23:46:59 +0700

    33

--- 04-05-2025/elementTerkecilK2.go
+++ 04-05-2025/elementTerkecilK2.go
@@ -5,18 +5,15 @@ import "fmt"
 func main() {
 	data := [...]int{8, 3, 7, 1, 4}
 
-	// Inisialisasi dengan nilai maksimum dari integer
 	min1 := data[0]
-	min2 := 9999999 // asumsi angka sangat besar
+	min2 := 9999999
 
-	// Cari nilai terkecil pertama
 	for i := 1; i < len(data); i++ {
 		if data[i] < min1 {
 			min1 = data[i]
 		}
 	}
 
-	// Cari nilai terkecil ke-2 (lebih besar dari min1)
 	for i := 0; i < len(data); i++ {
 		if data[i] > min1 && data[i] < min2 {
 			min2 = data[i]
