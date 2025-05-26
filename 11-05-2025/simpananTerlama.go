66da5e28063cee612e0ff7e2b1629e68e13cd670
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-05-26 23:59:22 +0700

    42

--- 11-05-2025/simpananTerlama.go
+++ 11-05-2025/simpananTerlama.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+
+	tabungang := []int{500, 700, 10, 10, 10, 0, 800, 900, 1000, 1100, 0, 200, 300, 400, 500}
+	a, tempat := 0, 0
+
+	for i := 0; i < len(tabungang); i++ {
+		if tabungang[i] != 0 {
+			a++
+			if a > tempat {
+				tempat = a
+			}
+		} else if tabungang[i] == 0 {
+			a = 0
+		}
+	}
+
+	fmt.Println(tempat)
+
 }
