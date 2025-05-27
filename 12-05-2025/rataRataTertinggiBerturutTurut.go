eb1a9d4ce9227b06d58894d9b51818a31e43dec8
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-05-27 22:47:49 +0700

    44

--- 12-05-2025/rataRataTertinggiBerturutTurut.go
+++ 12-05-2025/rataRataTertinggiBerturutTurut.go
@@ -1,30 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	data := []int{100, 200, 150, 300, 250, 400, 100, 200, 300, 100}
 
-	penjualan := []int{100, 200, 150, 300, 250, 400, 100, 200, 300, 100}
+	maxAvg := 0.0
+	var maxSegment [3]int
 
-	rataTertinggi, hari, hasil := 0, 0, 0
+	for i := 0; i <= len(data)-3; i++ {
+		sum := data[i] + data[i+1] + data[i+2]
+		avg := float64(sum) / 3
 
-	for i := 0; i < len(penjualan)-1; i++ {
-		if penjualan[i] <= penjualan[i+1] {
-			hasil += penjualan[i]
-			hari++
-			if i == len(penjualan)-2 {
-				hasil += penjualan[i+1]
-				hari++
-			}
-			if hari > 0 && hasil/hari > rataTertinggi {
-				rataTertinggi = hasil / hari
-				fmt.Println(rataTertinggi)
-			}
-		} else {
-			hasil = 0
-			hari = 0
+		if avg > maxAvg {
+			maxAvg = avg
+			maxSegment = [3]int{data[i], data[i+1], data[i+2]}
 		}
 	}
-	println(rataTertinggi)
 
+	fmt.Printf("Rata-rata tertinggi: %.2f (dari %v)\n", maxAvg, maxSegment)
 }
