3a8beeb83a5318b1ca06d9468e57763cc417bc9b
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-06-28 20:31:53 +0700

    12_06_2025

---
+++ Java/12_07_2025/mendapatkanBola.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class mendapatkanBola {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Program Mendapatkan Bola");
+
+        System.out.println("Masukkan bola pertama: ");
+        String bolaPertama = input.nextLine();
+
+        System.out.println("Masukkan bola kedua: ");
+        String bolaKedua = input.nextLine();
+
+        System.out.println("Masukkan bola ketiga: ");
+        String bolaKetiga = input.nextLine();
+
+
+
+    }
+}
